679480e8f85faab96db90fa5fe3369b8d77b01dc
osierosavo <osierosavo@gmail.com>
2021-06-05 01:12:42 +0900

    포트폴리오 레이벤 수정

--- rayban/js/common.js
+++ rayban/js/common.js
@@ -1,3 +1,13 @@
 // 올해가 몇년인지 만들어내는 코드
 // const thisYear = document.querySelector('.this-year');
 // thisYear.textContent = new Date().getFullYear();
+
+const mobileSearch = document.querySelector('header .search');
+const backgroundM = document.querySelector('.mobile_background');
+const headerM = document.querySelector('header .inner');
+
+mobileSearch.addEventListener('click', function () {
+  console.log('works!')
+  backgroundM.style.backgroundPosition = '100vw, 0'
+  headerM.style.marginLeft = '90vw'
+});
