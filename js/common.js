d66968021ad6d28d930e2bc7f2597c96750b7243
osierosavo <osierosavo@gmail.com>
2021-06-05 15:22:10 +0900

    포트폴리오 탭메뉴 마진 수정 및 파일 분류

---
+++ js/common.js
@@ -0,0 +1,61 @@
+// 이메일 & 번호 복사
+const notification = document.querySelector('.copied');
+const hide = document.querySelector('.show');
+
+document.querySelector(".copy1").addEventListener("click", function(){
+  const tempElem = document.createElement('textarea');
+  tempElem.value = 'osierosavo@gmail.com';  
+  document.body.appendChild(tempElem);
+
+  tempElem.select();
+  document.execCommand("copy");
+  document.body.removeChild(tempElem);
+
+  notification.classList.add('show');
+
+  setTimeout(function() {
+    notification.classList.remove('show');
+  }, 600);
+
+  return;
+
+});
+
+document.querySelector(".copy2").addEventListener("click", function(){
+  const tempElem = document.createElement('textarea');
+  tempElem.value = '01039551248';  
+  document.body.appendChild(tempElem);
+
+  tempElem.select();
+  document.execCommand("copy");
+  document.body.removeChild(tempElem);
+  
+  notification.classList.add('show');
+
+  setTimeout(function() {
+    notification.classList.remove('show');
+  }, 600);
+
+  return;
+
+});
+
+// 메뉴 변경
+
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    });
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    });
+    target.classList.add('active')
+    tab.classList.add('active')
+  })
+})
